aca121ef29dcf1b4a9634e50b1bddc38733c4793
shwetap3000 <shwetaprasad30.05@gmail.com>
2026-01-08 17:48:11 +0530

    Solve minor error in views.py file

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('lost/', include('lost.urls')),
     path('found/', include('found.urls')),
+    path('api/accounts', include('accounts.urls')),
 ]
