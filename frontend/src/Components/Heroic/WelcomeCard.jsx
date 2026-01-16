79a2d233ef9f575a170fea08009496f1398df978
shwetap3000 <shwetaprasad30.05@gmail.com>
2026-01-16 12:28:43 +0530

    Improve welcome card

--- frontend/src/Components/Heroic/WelcomeCard.jsx
+++ frontend/src/Components/Heroic/WelcomeCard.jsx
@@ -1,21 +1,28 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import "../../Styles/WelcomeCard.css";
+import bag from "../../assets/bag.png";
+import { faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function WelcomeCard() {
   return (
     <div className="wrapper">
       <h2 className="card-heading">Welcome to Findora !</h2>
-      <p className="card-para">
-        The quickest way to reconnect lost items with their rightful owners.
-        Let's find what matters.
-      </p>
+      <div className="sub-wrapper">
+        <div>
+          <p className="card-para">
+            Reconnect lost items with rightful owners around the campus.
+          </p>
+        </div>
+        <div className="welcome-img">
+          <img src={bag} alt="bag-img" className="welcome-card-image" />
+        </div>
+      </div>
 
-
-      {/* here we will manage the navigation to the report item page or login page based on authentication status */}
-      
-      <Link to='/report' className="card-link">
-      <button className="card-btn">Report Item </button>
+      <Link to="/report" className="card-link">
+        <button className="card-btn">
+          <FontAwesomeIcon icon={faMagnifyingGlass} className="maginifying-glass" /> Report Item </button>
       </Link>
     </div>
   );
